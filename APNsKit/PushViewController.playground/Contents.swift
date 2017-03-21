2a43dc290fc1e6a9d5d45d4393dbc31299f1d842
hagmas <hgmsk1985@gmail.com>
2017-03-22 00:48:08 +0900

    Update playground

--- APNsKit/PushViewController.playground/Contents.swift
+++ APNsKit/PushViewController.playground/Contents.swift
@@ -1,3 +1,14 @@
+/*:
+
+ # How to use this playground
+ 
+ 1. Build the framework.
+ 2. Create the provider client certificate. (Follow the steps described [here](https://developer.apple.com/library/content/documentation/IDEs/Conceptual/AppDistributionGuide/AddingCapabilities/AddingCapabilities.html#//apple_ref/doc/uid/TP40012582-CH26-SW11))
+ 3. Export your identity from the keychain app in the Personal Information Exchange Format (.p12) and place it under the Resources folder of this playground.
+ 4. Fill port number, developement/production server, the device token of the target device, the name of the p12 file and its pass phrase.
+ 5. You will see a view on the playground timeline and push the button to send notifications.
+ */
+ 
 import UIKit
 import APNsKit
 import PlaygroundSupport
@@ -5,10 +16,11 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 URLCache.shared = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil)
 
+let payload = APNsPayload(body: "Hello, World!")
 let request = APNsRequest(port: <#port#>,
                           server: <#server#>,
                           deviceToken: <#deviceToken#>,
-                          payload: <#payload#>)
+                          payload: payload)
 
 let pushVC: PushViewController
 if let _pushVC = PushViewController(p12FileName: <#p12FileName#>,
